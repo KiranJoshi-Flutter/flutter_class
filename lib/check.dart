c87d3930b1514426601b64a66c74bfe0c9e9e08e
KiranJoshi52 <63843382+KiranJoshi52@users.noreply.github.com>
2021-06-24 06:58:18 +0545

    rows and column

---
+++ lib/check.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Check extends StatefulWidget {
+  const Check({Key key}) : super(key: key);
+
+  @override
+  _CheckState createState() => _CheckState();
+}
+
+class _CheckState extends State<Check> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
