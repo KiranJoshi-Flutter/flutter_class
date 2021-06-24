341adc3109f9651475c86e56279a1c37a3dd937b
KiranJoshi52 <63843382+KiranJoshi52@users.noreply.github.com>
2021-06-24 07:14:38 +0545

    Check2

---
+++ lib/check2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Check2 extends StatefulWidget {
+  const Check2({Key key}) : super(key: key);
+
+  @override
+  _Check2State createState() => _Check2State();
+}
+
+class _Check2State extends State<Check2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
