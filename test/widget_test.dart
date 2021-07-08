209d77d279409dac839bbde2b235d0a9528538ec
KiranJoshi52 <63843382+KiranJoshi52@users.noreply.github.com>
2021-07-08 08:50:56 +0545

    stack and stateful complete

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:containers/main.dart';
+import 'package:containers/container_ui.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
